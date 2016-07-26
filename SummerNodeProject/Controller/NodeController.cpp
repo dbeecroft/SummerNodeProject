aec68364ffb9f2cf51dc64c957b8a6d00b7df119
dbeecroft <teddie79@gmail.com>
2016-07-26 15:30:16 -0600

    New Node classes

---
+++ SummerNodeProject/Controller/NodeController.cpp
@@ -0,0 +1,19 @@
+//
+//  NodeController.cpp
+//  SummerNodeProject
+//
+//  Created by Denise Beecroft on 7/26/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#include "NodeController.h"
+
+void NodeController :: tryNodes()
+{
+    
+}
+
+void NodeController :: start()
+{
+    tryNodes();
+}
---
+++ SummerNodeProject/Controller/NodeController.h
@@ -0,0 +1,22 @@
+//
+//  NodeController.hpp
+//  SummerNodeProject
+//
+//  Created by Denise Beecroft on 7/26/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#ifndef NodeController_h
+#define NodeController_h
+
+#include <stdio.h>
+class NodeController
+{
+private:
+    void tryNodes();
+public:
+    void start();
+};
+
+
+#endif /* NodeController_h */
