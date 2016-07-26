167e63d41e7be8c28fc1b4742298bb9e48bb983e
dbeecroft <teddie79@gmail.com>
2016-07-26 15:56:24 -0600

    Created tryNodes

--- SummerNodeProject/Controller/NodeController.cpp
+++ SummerNodeProject/Controller/NodeController.cpp
@@ -7,10 +7,21 @@
 //
 
 #include "NodeController.h"
+#include "../Model/DataNode.cpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 void NodeController :: tryNodes()
 {
-    
+    DataNode<int> numberNode;
+    DataNode<string> wordNode;
+    cout << "Before" << endl;
+    cout << numberNode.getNodeData() << endl;
+    cout <<"After" << endl;
+    numberNode.setNodeData(231);
+    cout << numberNode.getNodeData() << endl;
 }
 
 void NodeController :: start()
--- SummerNodeProject/main.cpp
+++ SummerNodeProject/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  SummerNodeProject
 //
-//  Created by Henrichsen, Cody on 7/26/16.
+//  Created by Denise Beecroft on 7/26/16.
 //  Copyright © 2016 Denise Beecroft. All rights reserved.
 //
 
