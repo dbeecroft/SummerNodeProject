060abd69ea98709d4753f8938991d1a30dfc8a1a
dbeecroft <teddie79@gmail.com>
2016-07-28 08:31:15 -0600

    contains an if to check if isInList

--- SummerNodeProject/Model/SummerList.cpp
+++ SummerNodeProject/Model/SummerList.cpp
@@ -150,3 +150,23 @@ Type SummerList<Type> :: remove(int index)
     size--;
     return removedValue;
 }
+
+template <class Type>
+bool SummerList<Type> :: contains(Type data)
+{
+    bool isInList = false;
+    
+    DataNode<Type> * containsPointer = front;
+    
+    for(int index = 0; index < size; index++)
+    {
+        if(data == containsPointer->getNodeData())
+        {
+            isInList = true;
+        }
+        containsPointer = containsPointer->getNodePointer();
+    }
+    
+    
+    return isInList;
+}
