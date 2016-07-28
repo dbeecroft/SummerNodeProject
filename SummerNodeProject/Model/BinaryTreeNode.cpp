48e3a67bfc86e89b3cbdeca99396cbfcdc0b2a93
dbeecroft <teddie79@gmail.com>
2016-07-28 08:55:09 -0600

    Created BinaryTreeNode

---
+++ SummerNodeProject/Model/BinaryTreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeNode.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/28/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#include "BinaryTreeNode.h"
---
+++ SummerNodeProject/Model/BinaryTreeNode.h
@@ -0,0 +1,22 @@
+//
+//  BinaryTreeNode.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/28/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_h
+#define BinaryTreeNode_h
+
+#include <stdio.h>
+#include"DataNode.h"
+
+template <class Type>
+class BinaryTreeNode : public DataNode<Type>
+{
+    
+};
+
+
+#endif /* BinaryTreeNode_h */
