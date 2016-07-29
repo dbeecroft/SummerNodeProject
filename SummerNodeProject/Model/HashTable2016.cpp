c145f9fe60a8f4c64e66b82eccbdcf10b2616b49
dbeecroft <teddie79@gmail.com>
2016-07-29 09:26:11 -0600

    created HashTable

---
+++ SummerNodeProject/Model/HashTable2016.cpp
@@ -0,0 +1,10 @@
+//
+//  HashTable2016.cpp
+//  SummerNodeProject
+//
+//  Created by Denise Beecroft on 7/29/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#include "HashTable2016.h"
+
---
+++ SummerNodeProject/Model/HashTable2016.h
@@ -0,0 +1,21 @@
+//
+//  HashTable2016.h
+//  SummerNodeProject
+//
+//  Created by Denise Beecroft on 7/29/16.
+//  Copyright © 2016 Denise Beecroft. All rights reserved.
+//
+
+#ifndef HashTable2016_hpp
+#define HashTable2016_hpp
+
+#include <stdio.h>
+
+template<class Type>
+class HashTable
+{
+    
+};
+
+
+#endif /* HashTable2016_hpp */
